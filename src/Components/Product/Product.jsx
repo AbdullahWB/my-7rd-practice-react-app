4c72ab79a4d033a858dd1ebea9b19d7357ae88ec
AbdullahWB <abdullah917828@gmail.com>
2023-03-27 16:28:41 +0600

    show item and style

--- src/Components/Product/Product.jsx
+++ src/Components/Product/Product.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons'
 import './Product.css'
 
 const Product = (props) => {
-    console.log(props.product)
-    const {img, name, seller, quantity, price} = props.product
+    // console.log(props.product)
+    const { img, name, seller, quantity, price } = props.product;
+    const handleAddToCart = props.handleAddToCart;
     return (
         <div className='product'>
             <img src={img} alt="" />
@@ -13,7 +16,7 @@ const Product = (props) => {
                 <p>Manufacturer : {seller}</p>
                 <p>Rating :  {quantity}</p>
             </div>
-            <button>Add to Cart</button>
+            <button onClick={()=>handleAddToCart(props.product)}>Add to Cart <FontAwesomeIcon icon={faShoppingCart} /> </button>
         </div>
     );
 };
--- src/Components/Shop/Shop.jsx
+++ src/Components/Shop/Shop.jsx
@@ -4,23 +4,31 @@ import './Shop.css'
 
 const Shop = () => {
     const [products, setProducts] = useState([]);
+    const [cart, setCart] = useState([]);
     useEffect(() => {
         fetch('products.json')
             .then(res => res.json())
             .then(data => setProducts(data))
-    },[])
+    }, [])
+    const handleAddToCart = (product) => {
+        // console.log('product added to cart', product)
+        const newCart = [...cart, product]
+        setCart(newCart);
+    }
     return (
         <div className='shop-container'>
             <div className='product-container'>
                 {
                     products.map(product => <Product
                         key={product.id}
-                        product = {product}
+                        product={product}
+                        handleAddToCart = {handleAddToCart}
                     ></Product>)
                 }
             </div>
             <div className='cart-container'>
                 <h2>order</h2>
+                <p>selected item: {cart.length}</p>
             </div>
         </div>
     );
